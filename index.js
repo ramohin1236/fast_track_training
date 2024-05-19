c9eee682e4650460e08152346075329a0d485c2f
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-05-19 08:23:23 -0700

    click the main logo of the top of the page and relod its main page

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#redirect-link').click(function(event) {
+        event.preventDefault(); 
+        window.location.href = 'https://fasttracklifeuk.co.uk/';
+    });
+});
